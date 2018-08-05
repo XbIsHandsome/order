a2e1b6790ae4965ff5c6c382bfea3b4acaa9b2c7
xubing <954009381@qq.com>
2018-08-05 08:45:51 +0800

    上传框架的基本文件

---
+++ order/.idea/fileTemplates/internal/Enum.java
@@ -0,0 +1,10 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+/**
+ * @author     : 许兵
+ * @time       : ${YEAR}-${MONTH}-${DAY} ${HOUR}:${MINUTE}:${SECOND}
+ * @E-mail     : troubleme@aliyun.com
+ * @Wechat     ：玖弦与柒墨 
+ */
+public enum ${NAME} {
+}
